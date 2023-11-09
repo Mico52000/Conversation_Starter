2d4d31cb0a64b14249332bf19b9bcad7a421aa5a
Michael George Zakhary <61880954+Mico52000@users.noreply.github.com>
2023-11-09 19:13:09 +0200

    implemented output parser in order to guarantee output structure from the backend

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,31 +4,43 @@ from langchain.chains import LLMChain
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parser import person_intel_parser
 
 ### adding things in curly brackets allows us to inject information in prompt templates
 
 
 def ice_break(name: str) -> str:
-    linkedin_url = linkedin_lookup_agent("Renee Zakhary")
+    linkedin_url = linkedin_lookup_agent(name)
     print(linkedin_url)
     summary_template = """
-    Given the information {information} about a person, I want you to create:
-    1. A medium length summary
-    2. two interesting facts about them
-    """
-
+             given the Linkedin information {linkedin_information}  about a person from I want you to create:
+             1. a short summary
+             2. two interesting facts about them
+             3. A topic that may interest them
+             4. 2 creative Ice breakers to open a conversation with them 
+             \n{format_instructions}
+         """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["linkedin_information"], template=summary_template,
+        ### since our output parser is ready before running the chain, we can give it to the prompt template now
+        ### this is done by defining the partial variables
+        partial_variables = {"format_instructions":person_intel_parser.get_format_instructions()}
     )
 
-
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
     # notice how input_variables in the prompt template become an input name in chain.run()
-    res = chain.run(information=scrape_linkedin_profile(linkedin_url))
+    res = chain.run(linkedin_information=scrape_linkedin_profile(linkedin_url))
+    ### notice how the rsult is still a string but it is formatted exactly like the output parser we defined
+    ### in output parser.py
+
     print(res)
-    return res
+
+    return person_intel_parser.parse(res)
+
+
+if __name__ == "__main__":
+    ice_break("Michael Zakhary")
---
+++ output_parser.py
@@ -0,0 +1,22 @@
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class PersonIntel(BaseModel):
+    summary: str = Field(description="Summary of the person")
+    facts: List[str] = Field(description="Interesting facts about the person")
+    topics_of_interest: List[str] = Field(description="Topics that interset the person")
+    conversation_starters: List[str] = Field(
+        description="Create ince breakers to start a conversation with someone"
+    )
+
+    def to_dict(self):
+        return {
+            "summary": self.summary,
+            "facts": self.facts,
+            "topics_of_interest": self.topics_of_interest,
+            "conversation_starters": self.conversation_starters,
+        }
+
+person_intel_parser = PydanticOutputParser(pydantic_object = PersonIntel)
